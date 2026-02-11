e1e7ace71a8217f41ed2403ea28df879cd013c64
caiotleal <168606349+caiotleal@users.noreply.github.com>
2026-02-10 23:53:34 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: './', // Isso garante que o site funcione em qualquer caminho do servidor
   build: {
     outDir: 'dist',
-    emptyOutDir: true,
-    sourcemap: false,
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-      },
-    },
-  },
-  server: {
-    port: 3000
   }
-});
+})
